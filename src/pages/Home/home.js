bd15c873f77632057cb846ddb1a577eff049a0f9
CalebOkyere <agyakojo4321@gmail.com>
2022-04-14 23:03:51 +0000

    lets begin homepage

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -10,29 +10,6 @@ export default class Home extends Component {
     this.state = {};
   }
   render() {
-    return (
-      <div className="flex w-screen ">
-        <div className="w-1/4">
-          {" "}
-          <SideNav />
-        </div>
-        <div className="w-3/4">
-          {/* search */}
-          <div className="flex justify-around mt-16 mb-10 mr-16 h-12 ">
-            <SearchBar />
-            <OrderButton />
-          </div>
-          {/* category */}
-          <div>
-            <CategoryItem name="Dinner" icon={<MdOutlineFastfood />} />
-          </div>
-          {/* Dinner Inspiration */}
-          <div></div>
-          {/* Promotions */}
-          <div></div>
-        </div>
-        {/* <div className="w-1/4"></div> */}
-      </div>
-    );
+    return <div className=""></div>;
   }
 }
